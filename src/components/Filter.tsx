f97579123f3b97bba8736d3ab566a7927b344c74
Gustavo Arruda <gustavoleone3456@hotmail.com>
2024-01-25 16:52:48 -0300

    label dos inputs do filtro

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -41,7 +41,7 @@ const Filter: React.FC<FilterProps> = ({ onFilter }) => {
       </Menu.Item>
       <Menu.Item key="date">
         <Checkbox checked={selectedFilters.includes('date')}>
-          Filtrar por Data de Inclusão
+          Filtrar por Período
         </Checkbox>
       </Menu.Item>
     </Menu>
@@ -50,23 +50,31 @@ const Filter: React.FC<FilterProps> = ({ onFilter }) => {
   return (
     <Row justify="center">
       <Col span={100}>
-        <Space style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', gap: '1em' }}>
+        <Space style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'flex-end', gap: '1em' }}>
           <Dropdown overlay={menu} placement="bottomCenter" trigger={['click']}>
-            <Button>
+            <Button className='DropDownfilter'>
               Filtros <DownOutlined />
             </Button>
           </Dropdown>
           {selectedFilters.includes('name') && (
-            <Input
-              onInput={handleFilter}
-              placeholder="Filtrar por nome"
-              value={nameFilter}
-              onChange={(e) => setNameFilter(e.target.value)}
-            />
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '.3em' }}>
+              <label style={{ fontSize: '13px' }} htmlFor="nameFilter">Buscar por Nome:</label>
+              <Input
+                id="nameFilter"
+                name="nameFilter"
+                onInput={handleFilter}
+                placeholder="Filtrar por nome"
+                value={nameFilter}
+                onChange={(e) => setNameFilter(e.target.value)}
+              />
+            </div>
           )}
           {selectedFilters.includes('price') && (
-            <>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '.3em', border: '1px solid #b8b9ba', padding: '.5em', borderRadius: '5px' }}>
+              <label htmlFor="priceFilter">Buscar por Preço:</label>
               <Input
+                id="priceFilter"
+                name="priceFilter"
                 type="range"
                 min={0}
                 max={10000}
@@ -75,16 +83,28 @@ const Filter: React.FC<FilterProps> = ({ onFilter }) => {
                 onChange={(e) => setPriceFilter(parseFloat(e.target.value))}
               />
               <span>A partir de: R$ {priceFilter}</span>
-            </>
+            </div>
           )}
           {selectedFilters.includes('date') && (
             <>
-              <Input
-                type="date"
-                onChange={(e) => setStartDateFilter(e.target.value)} />
-              <Input
-                type="date"
-                onChange={(e) => setEndDateFilter(e.target.value)} />
+              <div style={{ display: 'flex', flexDirection: 'column', gap: '.3em' }}>
+
+                <label htmlFor="startDateFilter">Data Inicial:</label>
+                <Input
+                  id="startDateFilter"
+                  name="startDateFilter"
+                  type="date"
+                  onChange={(e) => setStartDateFilter(e.target.value)} />
+              </div>
+              <div style={{ display: 'flex', flexDirection: 'column', gap: '.3em' }}>
+
+                <label htmlFor="endDateFilter">Data Final:</label>
+                <Input
+                  id="endDateFilter"
+                  name="endDateFilter"
+                  type="date"
+                  onChange={(e) => setEndDateFilter(e.target.value)} />
+              </div>
             </>
           )}
           {selectedFilters.length > 0 && (
